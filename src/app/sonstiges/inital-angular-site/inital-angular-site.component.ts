2b2df86f10fc1c962165e05fa745802299604307
SebPortfolio <Sebastian.Koch@Student.HTW-Berlin.de>
2026-02-11 12:59:07 +0100

    added brands package for FontAwesome and put own GitHub on initial angular site

--- src/app/sonstiges/inital-angular-site/inital-angular-site.component.ts
+++ src/app/sonstiges/inital-angular-site/inital-angular-site.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
     selector: 'app-inital-angular-site',
     standalone: true,
-    imports: [],
+    imports: [FontAwesomeModule],
     templateUrl: './inital-angular-site.component.html',
     styleUrl: './inital-angular-site.component.less',
 })
 export class InitalAngularSiteComponent {
+    protected readonly faGithub = faGithub;
     title = 'four-in-a-row';
 }
