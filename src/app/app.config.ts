7d4bb2b697fb0941db4d359ca92bdcf22fc80c81
SebPortfolio <Sebastian.Koch@Student.HTW-Berlin.de>
2026-02-18 12:39:07 +0100

    corrected traslation providing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { BASE_PATH } from '../../openapi';
 import { routes } from './app.routes';
 import { apiErrorInterceptor } from './sonstiges/api-error.interceptor';
+import { HttpLoaderFactory } from './sonstiges/translate-http-loader';
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -12,5 +14,15 @@ export const appConfig: ApplicationConfig = {
         provideRouter(routes, withComponentInputBinding()),
         provideHttpClient(withInterceptors([apiErrorInterceptor])),
         { provide: BASE_PATH, useValue: 'http://localhost:8000' },
+        importProvidersFrom(
+            TranslateModule.forRoot({
+                loader: {
+                    provide: TranslateLoader,
+                    useFactory: HttpLoaderFactory,
+                    deps: [HttpClient],
+                },
+                defaultLanguage: 'de',
+            })
+        ),
     ],
 };
